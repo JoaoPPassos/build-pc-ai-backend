a0a7eab348071a054792582a670eb4699dbc6867
JoaoPPassos <joao.passos@teamsoft.com.br>
2025-10-04 11:44:48 -0300

    feat: adicionando teste paginação kabum

--- tests/integration/product.spec.ts
+++ tests/integration/product.spec.ts
@@ -1,13 +1,19 @@
-import puppeteer, { Browser, Page } from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import type { Browser, Page } from "puppeteer";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { ProductScrapper } from "../../src/infrastructure/scrappers/ProductScrapper";
 
 // Timeout maior para testes E2E, já que acessa a internet
 jest.setTimeout(400000);
+puppeteer.use(StealthPlugin());
 
 describe("GPU Scraper - Integração", () => {
   let browser: Browser;
   let page: Page;
   const scrapper = new ProductScrapper();
+  const kabumUrl =
+    "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=1&page_size=100&facet_filters=&sort=most_searched";
+  const pichauURL = "https://www.pichau.com.br/hardware/placa-de-video";
 
   beforeAll(async () => {
     browser = await puppeteer.launch({ headless: true });
@@ -48,18 +54,24 @@ describe("GPU Scraper - Integração", () => {
   };
 
   it("deve extrair GPUs reais da Kabum", async () => {
-    const kabumURL =
-      "https://www.kabum.com.br/hardware/placa-de-video-vga";
-    await runIntegrationTest(kabumURL, "parsePageKabum", "kabum", "Placa de Vídeo");
+    await runIntegrationTest(
+      kabumUrl,
+      "parsePageKabum",
+      "kabum",
+      "Placa de Vídeo"
+    );
   });
 
   it("deve extrair GPUs reais da Pichau", async () => {
-    const pichauURL = "https://www.pichau.com.br/hardware/placa-de-video";
-    await runIntegrationTest(pichauURL, "parsePagePichau", "pichau", "Placa de Video");
+    await runIntegrationTest(
+      pichauURL,
+      "parsePagePichau",
+      "pichau",
+      "Placa de Video"
+    );
   });
 
   it("deve percorrer múltiplas páginas na Pichau", async () => {
-    const pichauURL = "https://www.pichau.com.br/hardware/placa-de-video";
     await page.goto(pichauURL, { waitUntil: "networkidle2" });
 
     const products = await scrapper.parsePagePichau(page);
@@ -68,5 +80,16 @@ describe("GPU Scraper - Integração", () => {
     const firstProduct = products[0];
     expect(firstProduct.code).toBeTruthy();
     expect(firstProduct.title).toContain("Placa de Video");
+  });
+
+  it("deve percorrer múltiplas páginas na Kabum", async () => {
+    await page.goto(kabumUrl, { waitUntil: "networkidle2" });
+
+    const products = await scrapper.parsePageKabum(page);
+
+    expect(products.length).toBeGreaterThan(100); // Exemplo: espera mais de 20 produtos se houver paginação
+    const firstProduct = products[0];
+    expect(firstProduct.code).toBeTruthy();
+    expect(firstProduct.title).toContain("Placa de Vídeo");
   });
 });
