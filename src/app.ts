3afa32c1a9d756146089fb51def21adbffc1e400
JoaoPPassos <joao.passos@teamsoft.com.br>
2025-10-05 18:16:44 -0300

    fix: ajustando css e js urls

--- src/app.ts
+++ src/app.ts
@@ -22,10 +22,12 @@ const limiter = rateLimit({
 });
 const swaggerUiOptions = {
   explorer: true,
-  customCss:
-    ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
   customCssUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.min.css",
+  customJs: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.js",
+  ],
 };
 
 connectDatabase();
